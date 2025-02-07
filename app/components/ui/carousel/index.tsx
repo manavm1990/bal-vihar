aa48af10447b5d9887a60d963e98f4f841146bf7
Manav Misra <manavm@visionwebsoft.com>
2025-02-06 22:53:55 -0600

    Hide carousel 🎠 navigation buttons for touch

--- app/components/ui/carousel/index.tsx
+++ app/components/ui/carousel/index.tsx
@@ -163,7 +163,7 @@ const CarouselPrevious = React.memo(
         variant={variant}
         size={size}
         className={cn(
-          'absolute h-8 w-8 rounded-full',
+          'pointer-coarse:hidden absolute h-8 w-8 rounded-full',
           orientation === 'horizontal'
             ? 'top-1/2 -left-12 -translate-y-1/2'
             : '-top-12 left-1/2 -translate-x-1/2 rotate-90',
@@ -190,7 +190,7 @@ const CarouselNext = React.memo(
         variant={variant}
         size={size}
         className={cn(
-          'absolute h-8 w-8 rounded-full',
+          'pointer-coarse:hidden absolute h-8 w-8 rounded-full',
           orientation === 'horizontal'
             ? 'top-1/2 -right-12 -translate-y-1/2'
             : '-bottom-12 left-1/2 -translate-x-1/2 rotate-90',
