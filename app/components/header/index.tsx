69c0c16600586e4bbb3a02881920947448a27a6b
Manav Misra <manavm@visionwebsoft.com>
2025-02-06 23:57:46 -0600

    Adjust logo size

--- app/components/header/index.tsx
+++ app/components/header/index.tsx
@@ -4,7 +4,14 @@ import { DesktopNav, MobileNav } from './navs'
 export default function Header() {
   return (
     <header className="relative flex items-center justify-center gap-x-4 p-4 shadow-lg xl:justify-between">
-      <Image src="/logo.svg" alt="Bal Vihar Logo" width={180} height={173} priority />
+      <Image
+        src="/logo.svg"
+        alt="Bal Vihar Logo"
+        width={180}
+        height={173}
+        priority
+        className="size-24 sm:size-32 md:size-40"
+      />
       <DesktopNav />
 
       <MobileNav />
