1226d1e1313a580e1da53dae29d24f5c09ab2fa2
Manav Misra <manavm@visionwebsoft.com>
2025-02-14 22:27:56 -0600

    :lipstick: Update H2

--- app/components/ui/typography/h2.tsx
+++ app/components/ui/typography/h2.tsx
@@ -5,7 +5,7 @@ export default function H2({ className, children }: TypographyProps) {
   return (
     <h2
       className={cn(
-        'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0',
+        'scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-wider first:mt-0',
         className,
       )}
     >
