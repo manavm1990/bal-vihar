bbdfb69845efa193337736c0773f2009af318dbe
Manav Misra <manavm@visionwebsoft.com>
2025-02-14 22:29:25 -0600

    refactor:

--- app/components/ui/icons/index.tsx
+++ app/components/ui/icons/index.tsx
@@ -1,3 +1,3 @@
-export { default as ThreeBars } from './3-bars'
-export { default as ChevronDown } from './chevron-down'
-export { default as X } from './x'
+export { default as ThreeBarsIcon } from './3-bars'
+export { default as ChevronDownIcon } from './chevron-down'
+export { default as XIcon } from './x'
