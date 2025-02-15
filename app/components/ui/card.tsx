b9faa435dd1e3c864b0a475ba752bad93c00e02d
Manav Misra <manavm@visionwebsoft.com>
2025-02-15 09:15:14 -0600

    Add card attribute.

---
+++ app/components/ui/card.tsx
@@ -0,0 +1,42 @@
+import { cn } from '@lib/utils'
+import { type HTMLAttributes } from 'react'
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  className?: string
+}
+
+const Card = ({ className, ...props }: Props) => (
+  <div
+    className={cn(
+      'bg-background text-foreground rounded-lg border shadow-sm',
+      'transition-transform hover:scale-105 active:scale-95',
+      className,
+    )}
+    {...props}
+  />
+)
+
+const CardHeader = ({ className, ...props }: Props) => (
+  <div className={cn('flex flex-col space-y-1.5 p-6', className)} {...props} />
+)
+
+const CardTitle = ({ className, ...props }: HTMLAttributes<HTMLHeadingElement>) => (
+  <h3
+    className={cn('text-foreground text-2xl leading-none font-semibold tracking-tight', className)}
+    {...props}
+  />
+)
+
+const CardDescription = ({ className, ...props }: HTMLAttributes<HTMLParagraphElement>) => (
+  <p className={cn('text-muted-foreground text-sm', className)} {...props} />
+)
+
+const CardContent = ({ className, ...props }: Props) => (
+  <div className={cn('p-6 pt-0', className)} {...props} />
+)
+
+const CardFooter = ({ className, ...props }: Props) => (
+  <div className={cn('flex items-center p-6 pt-0', 'mt-auto border-t', className)} {...props} />
+)
+
+export { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle }
