7074d8544f9f89784f6e72428721c514f120b11f
Manav Misra <manavm@visionwebsoft.com>
2025-03-09 09:01:38 -0500

    feat: improve text contrast and enhance Indian flag theme

--- app/components/ui/typography/ps/lead.tsx
+++ app/components/ui/typography/ps/lead.tsx
@@ -2,5 +2,5 @@ import { cn } from '@lib/utils'
 import type TypographyProps from '../typography.types'
 
 export default function LeadP({ className, children }: TypographyProps) {
-  return <p className={cn('text-muted-foreground text-xl', className)}>{children}</p>
+  return <p className={cn('text-xl text-gray-600', className)}>{children}</p>
 }
