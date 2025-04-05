de819a287be6962f1024e76a061dad209b3c5897
Manav Misra <manavm@visionwebsoft.com>
2025-04-05 08:54:48 -0500

    feat: add `Article` component

---
+++ app/components/article.tsx
@@ -0,0 +1,24 @@
+import { H2 } from './ui/typography'
+import { cn } from '../lib/utils'
+
+interface ArticleProps {
+  title: string
+  children: React.ReactNode
+  className?: string
+}
+
+export default function Article({ title, children, className }: ArticleProps) {
+  return (
+    <article className="space-y-4">
+      <H2>{title}</H2>
+      <div
+        className={cn(
+          'columns-1 space-y-6 py-4 md:columns-2 md:gap-x-12 [&_p]:mt-2 [&_p]:text-pretty',
+          className,
+        )}
+      >
+        {children}
+      </div>
+    </article>
+  )
+}
