17a89ba6906c5133261b27a08e9b33c8cf56d9cb
Manav Misra <manavm@visionwebsoft.com>
2025-04-13 22:27:28 -0500

    Add footer for tables

--- app/components/ui/typography/table/index.tsx
+++ app/components/ui/typography/table/index.tsx
@@ -1,15 +1,18 @@
 import Tbody, { type TbodyProps } from './tbody'
 import Thead, { type TheadProps } from './thead'
 
-interface TableProps extends TheadProps, TbodyProps {}
+interface TableProps extends TheadProps, TbodyProps {
+  footer?: React.ReactNode
+}
 
-export default function Table({ headers, rows }: TableProps) {
+export default function Table({ headers, rows, footer }: TableProps) {
   return (
     <div className="my-4 w-full overflow-y-auto">
       <table className="w-full">
         <Thead headers={headers} />
         <Tbody rows={rows} />
       </table>
+      {footer && <div className="mt-4">{footer}</div>}
     </div>
   )
 }
