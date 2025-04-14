b839333579e6cdafc2c2a844f05e43558ab41909
Manav Misra <manavm@visionwebsoft.com>
2025-04-13 22:26:48 -0500

    Remove unnecessary fragment tag

--- app/about-us/administrative-team/page.tsx
+++ app/about-us/administrative-team/page.tsx
@@ -14,62 +14,60 @@ export const metadata: Metadata = {
 
 export default function AdministrativeTeamPage() {
   return (
-    <>
-      <Article title={TITLE}>
-        <P>
-          Bal Vihar of St. Louis has a well organized structure comprised of different teams working
-          together for smooth delivery of education, execution of operations and various events.
-          These individuals are all dedicated volunteers.
-        </P>
+    <Article title={TITLE}>
+      <P>
+        Bal Vihar of St. Louis has a well organized structure comprised of different teams working
+        together for smooth delivery of education, execution of operations and various events. These
+        individuals are all dedicated volunteers.
+      </P>
 
-        <P>The Organization consists of: </P>
+      <P>The Organization consists of: </P>
 
-        <ul>
-          <li>
-            <Strong>Executive Committee</Strong> - oversees the organization and provide the overall
-            guidance and support.
-          </li>
-          <li>
-            <Strong>Education</Strong> - This team oversees the class room functions, curriculum,
-            student and parent communication.
-          </li>
-          <li>
-            <Strong>Events</Strong> - Planned and newly adopted events are planned and executed by
-            this team.
-          </li>
-          <li>
-            <Strong>Community Projects</Strong> - Community Projects and Service to the community is
-            an integral part of our curriculum for our children in Youth Group.
-          </li>
-          <li>
-            <Strong>Operations</Strong> - This team oversees day to day operations and planning.
-          </li>
-          <li>
-            <Strong>Website</Strong> - This team oversees website activities, e-mails, photos and
-            maintenance.
-          </li>
-          <li>
-            <Strong>Finance</Strong> - This team addresses all operational details of financial
-            transactions, prepares budget, and handles treasury functions.
-          </li>
-          <li>
-            <Strong>Marketing & Communications</Strong> - This team is responsible for publishing
-            newsletters, submitting events related articles to various news agencies, marketing Bal
-            Vihar&apos;s events etc.
-          </li>
-          <li>
-            <Strong>Fundraising</Strong> - This team oversees Fundraising activities both in-house
-            and outside in the community.
-          </li>
-          <li>
-            <Strong>Youth</Strong> - This team oversees the youth class room activity, curriculum,
-            arrange and execute community projects and arrange field trips.
-          </li>
-          <li>
-            <Strong>Legal</Strong> - provides expert legal advice to Bal Vihar.
-          </li>
-        </ul>
-      </Article>
-    </>
+      <ul>
+        <li>
+          <Strong>Executive Committee</Strong> - oversees the organization and provide the overall
+          guidance and support.
+        </li>
+        <li>
+          <Strong>Education</Strong> - This team oversees the class room functions, curriculum,
+          student and parent communication.
+        </li>
+        <li>
+          <Strong>Events</Strong> - Planned and newly adopted events are planned and executed by
+          this team.
+        </li>
+        <li>
+          <Strong>Community Projects</Strong> - Community Projects and Service to the community is
+          an integral part of our curriculum for our children in Youth Group.
+        </li>
+        <li>
+          <Strong>Operations</Strong> - This team oversees day to day operations and planning.
+        </li>
+        <li>
+          <Strong>Website</Strong> - This team oversees website activities, e-mails, photos and
+          maintenance.
+        </li>
+        <li>
+          <Strong>Finance</Strong> - This team addresses all operational details of financial
+          transactions, prepares budget, and handles treasury functions.
+        </li>
+        <li>
+          <Strong>Marketing & Communications</Strong> - This team is responsible for publishing
+          newsletters, submitting events related articles to various news agencies, marketing Bal
+          Vihar&apos;s events etc.
+        </li>
+        <li>
+          <Strong>Fundraising</Strong> - This team oversees Fundraising activities both in-house and
+          outside in the community.
+        </li>
+        <li>
+          <Strong>Youth</Strong> - This team oversees the youth class room activity, curriculum,
+          arrange and execute community projects and arrange field trips.
+        </li>
+        <li>
+          <Strong>Legal</Strong> - provides expert legal advice to Bal Vihar.
+        </li>
+      </ul>
+    </Article>
   )
 }
