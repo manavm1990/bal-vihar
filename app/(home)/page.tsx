439d3c6bbff08620924c1b8f49eca9a52c67b8b9
Manav Misra <manavm@visionwebsoft.com>
2025-04-22 07:45:23 -0500

    Co-locate 'home page' card

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
-import Card from '@components/card'
 import { H2, H3, LeadP, P, Strong } from '@components/ui/typography'
 
+import { cn } from '../lib/utils'
 import { HeroCarousel } from './hero-carousel'
 
 export default function HomePage() {
@@ -14,19 +14,40 @@ export default function HomePage() {
           Registration for new and existing students is now open. Classes fill up fast, so
           don&apos;t wait to register!
         </LeadP>
+
         <P>
           Bal Vihar of St. Louis has been serving the Metro St. Louis Indian community{' '}
           <Strong>since 1992.</Strong>
         </P>
+
         <P>
           Bal Vihar of St. Louis will admit any student aged 5 through 18 based on the grade level
           they are enrolled at regular school regardless of their backgrounds and expect all to
           abide by the policies and procedures.
         </P>
+
         <a href="https://registration.balviharstlouis.org/">
           <Strong>Register now!</Strong>
         </a>
       </Card>
     </div>
+  )
+}
+
+interface CardProps {
+  children: React.ReactNode
+  className?: string
+}
+
+function Card({ children, className }: CardProps) {
+  return (
+    <section
+      className={cn(
+        'container mx-auto flex max-w-prose flex-col gap-4 rounded-lg border-l-4 border-l-[var(--color-primary)] bg-[var(--color-muted)] px-4 py-4',
+        className,
+      )}
+    >
+      {children}
+    </section>
   )
 }
