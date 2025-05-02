737f78bd02cca7290ebe65038d93c64c12a78a20
Manav Misra <manavm@visionwebsoft.com>
2025-05-02 09:37:35 -0500

    fix: remove unnecessary margin from H3 component

--- app/components/ui/typography/h3.tsx
+++ app/components/ui/typography/h3.tsx
@@ -4,7 +4,7 @@ import type TypographyProps from './typography.types'
 
 export default function H3({ className, children }: TypographyProps) {
   return (
-    <h3 className={cn('mt-2 scroll-m-20 text-2xl font-semibold tracking-tight', className)}>
+    <h3 className={cn('scroll-m-20 text-2xl font-semibold tracking-tight', className)}>
       {children}
     </h3>
   )
